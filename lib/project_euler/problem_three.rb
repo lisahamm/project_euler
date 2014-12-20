0893d67d894975c46fe353a32735a2195c0d1092
Lisa Hamm <lisalhamm@gmail.com>
2014-12-20 16:53:16 -0500

    Solution to Problem 3

--- lib/project_euler/problem_three.rb
+++ lib/project_euler/problem_three.rb
@@ -1,7 +1,15 @@
 module ProjectEuler
   module ProblemThree
     def self.largest_prime_factor(n)
+      prime_factors(n).last
+    end
 
+    private
+
+    def self.prime_factors(n)
+      return [] if n == 1
+      factor = (2..n).find {|x| n % x == 0}
+      [factor] + self.prime_factors(n/factor)
     end
   end
 end
--- spec/project_euler/problem_three_spec.rb
+++ spec/project_euler/problem_three_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 module ProjectEuler
   describe ProblemThree do
     it "returns the largest prime factor of a given number" do
-      expect(ProblemThree.largest_prime_factor(13195)).to eq 29
+      expect(ProblemThree.largest_prime_factor(6)).to eq 3
     end
+    it "returns the largest prime factor of a given number" do
+      expect(ProblemThree.largest_prime_factor(8)).to eq 2
+    end
+    # it "returns the largest prime factor of a given number" do
+    #   expect(ProblemThree.largest_prime_factor(13195)).to eq 29
+    # end
   end
 end
